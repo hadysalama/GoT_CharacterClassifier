b1d07c17f3e3a07e4f3a2abadfb15d6fe6573500
hadysalama <salama.hady@hotmail.com>
2019-07-14 20:30:46 -0400

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = 'mysite/GoT_CharacterClassifier/static/'
+STATIC_URL = 'project/src/mysite/GoT_CharacterClassifier/static/'
 
 # HTTPS Security
 # python manage.py runsslserver
