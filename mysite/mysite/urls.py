c3ea20aea3f14735ee6172aed33c0db7829ca1fa
Hady Salama <salama.hady@hotmail.com>
2020-02-27 01:12:15 -0500

    Update urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
      # ex: /polls/
     path('GoT_CharacterClassifier/', include('GoT_CharacterClassifier.urls')),
     # path('admin/', admin.site.urls),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
